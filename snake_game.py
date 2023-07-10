122f66b8b58960168820e282a014b9545980f83c
Wotah Man <55237095+wotahman@users.noreply.github.com>
2023-07-10 20:12:54 +1000

    adjusted code structure and created snake

--- snake_game.py
+++ snake_game.py
@@ -1,22 +1,51 @@
 import pygame
+import numpy as np
 
-#creating game screen
-windowRes = (512, 512)
+'''
+varibles & params
+'''
+#game screen params
+res = 512
+gridSize = 16
+
+#snake params
+blue = (0, 0, 255)
+red = (255, 0, 0)
+
+
+'''
+game initialization
+'''
+windowRes = (res, res)
+print("game resolution set to: ", windowRes)
 
 pygame.init()
 gameDisplay = pygame.display.set_mode(windowRes)
-pygame.display.update()
 pygame.display.set_caption("Snake Game :D")
+
+pygame.draw.rect(gameDisplay, blue, [res/2, res/2, gridSize, gridSize]) #set snake head position to center 
+pygame.display.update()
 
 gameOver = False #the game over state
 
+
+'''
+game running
+'''
 while not gameOver:
     for event in pygame.event.get():
-        print(event)
-
+        #print(event)
         #quit the game if the quit button on the window is pressed
         if event.type == pygame.QUIT: 
             gameOver = True
+            
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                print("snake goes LEFT")
+            
+        pygame.draw.rect(gameDisplay, blue, [res/2, res/2, gridSize, gridSize])
+        pygame.display.update()
+
 
 #quit the game
 pygame.display.quit()
