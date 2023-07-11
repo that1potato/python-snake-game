862db2a5f362c9ae43b821ca6aa634c9e7228da4
Wotah Man <55237095+wotahman@users.noreply.github.com>
2023-07-11 14:06:52 +1000

    optimized code structure

--- snake_game.py
+++ snake_game.py
@@ -19,6 +19,9 @@ black = (0, 0, 0)
 helpers
 '''
 def message(msg,color):
+    '''
+    initialize a message dialog
+    '''
     mesg = font_style.render(msg, True, color)
     gameDisplay.blit(mesg, [0, 0])
 
@@ -28,6 +31,39 @@ def quitGameDetection():
     '''
     if event.type == pygame.QUIT:
         quit()
+
+def snakeControlDetection():
+    '''
+    control snake movement with UP, DOWN, LEFT, RIGHT
+    '''
+    global dx, dy
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_LEFT:
+            dx = -gridSize
+            dy = 0
+            print("snake going LEFT")
+        if event.key == pygame.K_RIGHT:
+            dx = gridSize
+            dy = 0
+            print("snake going RIGHT")
+        if event.key == pygame.K_UP:
+            dx = 0
+            dy = -gridSize
+            print("snake going UP")
+        if event.key == pygame.K_DOWN:
+            dx = 0
+            dy = gridSize
+            print("snake going DOWN")
+
+def gameRules():
+    '''
+    defines the game rules,
+    any unsatisfied condition should cause gameOver = True
+    '''
+    global gameOver
+    #ends the game if snake bumps into the bundary
+    if x >= res or x < 0 or y >= res or y < 0:
+        gameOver = True
 
 
 '''
@@ -41,7 +77,7 @@ while restart:
     windowRes = (res, res)
     gameDisplay = pygame.display.set_mode(windowRes)
     pygame.display.set_caption("Snake Game :D")
-    font_style = pygame.font.SysFont(None, 50)
+    font_style = pygame.font.SysFont(None, 35)
 
     gameOver = False #the game over state
     restart = False
@@ -67,28 +103,9 @@ while restart:
         for event in pygame.event.get():
             #print(event)
             quitGameDetection()
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    dx = -gridSize
-                    dy = 0
-                    print("snake goes LEFT")
-                if event.key == pygame.K_RIGHT:
-                    dx = gridSize
-                    dy = 0
-                    print("snake goes RIGHT")
-                if event.key == pygame.K_UP:
-                    dx = 0
-                    dy = -gridSize
-                    print("snake goes UP")
-                if event.key == pygame.K_DOWN:
-                    dx = 0
-                    dy = gridSize
-                    print("snake goes DOWN")
+            snakeControlDetection()
                 
-        #ends the game if snake bumps into the bundary
-        if x >= res or x < 0 or y >= res or y < 0:
-            gameOver = True
+        gameRules()
 
         x += dx
         y += dy
